dfa8ada79d8744a42f748fe990585b0c5595b385
Sellers Helicent <sellersh@student.21-school.ru>
2024-05-29 21:50:06 +0300

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {ClientHomeComponent} from "./components/client/client-home/client-home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ClientHomeComponent }
+];
---
+++ src/app/components/psy/psy-home/psy-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-psy-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './psy-home.component.html',
+  styleUrl: './psy-home.component.scss'
+})
+export class PsyHomeComponent {
+
+}

0f2dc111f39cf729d5f63f0c600d71b287b0193d
Sellers Helicent <sellersh@student.21-school.ru>
2024-05-30 21:55:31 +0300

    g c login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import {ClientHomeComponent} from "./components/client/client-home/client-home.component";
+import { ClientLoginComponent } from './components/client-login/client-login.component';
 
 export const routes: Routes = [
-  { path: '', component: ClientHomeComponent }
+  { path: 'client', component: ClientHomeComponent },
+  { path: 'client/login', component: ClientLoginComponent },
+  // { path: '', component: ClientHomeComponent, redirectTo: 'client' },
+  { path: '**', component: ClientHomeComponent, redirectTo: 'client' }
 ];
